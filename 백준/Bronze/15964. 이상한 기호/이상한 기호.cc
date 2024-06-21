0688768b5065b8a8eb44ba1f121dfe35d5415ea2
Joey Lee <leejoey0921@gmail.com>
2024-06-22 01:55:09 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int A, B; cin >> A >> B;
+    cout << (A+B)*(A-B);
+
+    return 0;
+}
